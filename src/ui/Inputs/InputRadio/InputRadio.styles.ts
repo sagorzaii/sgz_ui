974e9a1ffc6edb565e68f582ab65223472cf73ca
Tian Sheng <siewtiansheng@gmail.com>
2023-09-28 23:05:57 +0800

    update input radio

--- src/ui/Inputs/InputRadio/InputRadio.styles.ts
+++ src/ui/Inputs/InputRadio/InputRadio.styles.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
 import { Input } from "../SharedInput.styles";
 
-export const InputRadio = styled(Input)<{ isSquared?: boolean }>``;
+export const InputRadio = styled(Input)<{ isSquared?: boolean }>`
+  margin: 0px 10px 0px 0px;
+`;
